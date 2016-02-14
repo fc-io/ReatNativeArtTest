93f32254776a48183518ce1e38eb56c4f3c61b7c
Fredrik Carlsson <me@fc.io>
2016-02-14 10:50:09 +0100

    some cleanup

--- index.ios.js
+++ index.ios.js
@@ -49,21 +49,20 @@ var FireworkShooter = React.createClass({
       this.setState(state => {
         var lineToUpdate = state.points.pop()
         var newLine = lineToUpdate.concat({x: latestPoint.x, y: latestPoint.y})
-        console.log('newLine')
-        if (pollTime % (16 * 2) === 0) {
-          console.log('pollTime')
+
+        if (pollTime % (8 * 2) === 0) {
           newLine.shift()
         }
         var points = state.points.concat([newLine])
 
-        pollTime += 16
+        pollTime += 8
 
         return {
           points
         }
       })
       this.poll()
-    }, 16)
+    }, 8)
   },
   move: function ({nativeEvent}) {
     latestPoint = {x: nativeEvent.pageX, y: nativeEvent.pageY}
@@ -78,33 +77,17 @@ var FireworkShooter = React.createClass({
     return threshold < 0.09 ? 0 : 1
   },
   render: function() {
-    var line = this.state.points[this.state.points.length - 1]
-    var smoothLine = line.reduce((acc, cV, i, line) => {
-      const previousPoint2 = line[i]
-      const previousPoint1 = line[i + 1]
-      const currentPoint = line[i + 2]
-
-      if (!currentPoint) {
-        return acc
-      }
-
-      const mid1 = getMidPoint(previousPoint1, previousPoint2)
-      const mid2 = getMidPoint(currentPoint, previousPoint1)
-
-      return acc.concat({
-        previousPoint1,
-        mid1,
-        mid2
-      })
-    }, [])
+    var lastLine = this.state.points[this.state.points.length - 1]
+    var smoothLine = getSmoothLine(lastLine)
 
     return (
       <View style={styles.container} {...this.panResponder.panHandlers}>
         <Surface width={width} height={height}>
           {
             smoothLine.map((points, i, array) =>
-              <AnimatedCircle
+              <Shape
                 key={i}
+                d={getLineSegmentPath(points)}
                 points={points}
                 strokeCap="round"
                 strokeJoin="miter"
@@ -119,20 +102,27 @@ var FireworkShooter = React.createClass({
   }
 });
 
-var getMidPoint = (p1, p2) => {
-    return {
-      x: (p1.x + p2.x) * 0.5,
-      y: (p1.y + p2.y) * 0.5
-    }
-}
+var getMidPoint = (p1, p2) => ({
+  x: (p1.x + p2.x) * 0.5,
+  y: (p1.y + p2.y) * 0.5
+})
 
-var AnimatedCircle = React.createClass({
-  render: function() {
-    const {previousPoint1, mid1, mid2} = this.props.points
-    const path = Path().moveTo(mid1.x, mid1.y).curveTo(previousPoint1.x, previousPoint1.y, mid2.x, mid2.y)
+var getSmoothLine = line => line.reduce((acc, cV, i, line) => {
+  const previousPoint2 = line[i]
+  const previousPoint1 = line[i + 1]
+  const currentPoint = line[i + 2]
 
-    return React.createElement(AnimatedShape, React.__spread({},  this.props, {d: path}));
+  if (!currentPoint) {
+    return acc
   }
-});
+
+  const mid1 = getMidPoint(previousPoint1, previousPoint2)
+  const mid2 = getMidPoint(currentPoint, previousPoint1)
+
+  return acc.concat({previousPoint1, mid1, mid2})
+}, [])
+
+var getLineSegmentPath = ({previousPoint1, mid1, mid2}) =>
+  Path().moveTo(mid1.x, mid1.y).curveTo(previousPoint1.x, previousPoint1.y, mid2.x, mid2.y)
 
 AppRegistry.registerComponent('native_canvas', () => FireworkShooter);
