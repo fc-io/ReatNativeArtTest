3234a4fa965b705fec96c2ab5c1b0132fe75ef11
Fredrik Carlsson <me@fc.io>
2016-02-14 12:57:53 +0100

    do some more cleanup

--- index.ios.js
+++ index.ios.js
@@ -15,7 +15,37 @@ let isPressing = false
 let pollTime = 0
 let pollId
 
-var FireworkShooter = React.createClass({
+const getAlpha = (i, numberOfPoints) => {
+  const threshold =  (numberOfPoints / (numberOfPoints * (numberOfPoints - i)))
+
+  return threshold < 0.09 ? 0 : 1
+}
+
+const getMidPoint = (p1, p2) => ({
+  x: (p1.x + p2.x) * 0.5,
+  y: (p1.y + p2.y) * 0.5
+})
+
+const getSmoothLine = line => line.reduce((acc, cV, i, line) => {
+  const previousPoint2 = line[i]
+  const previousPoint1 = line[i + 1]
+  const currentPoint = line[i + 2]
+
+  if (!currentPoint) {
+    return acc
+  }
+
+  const mid1 = getMidPoint(previousPoint1, previousPoint2)
+  const mid2 = getMidPoint(currentPoint, previousPoint1)
+
+  return acc.concat({previousPoint1, mid1, mid2})
+}, [])
+
+const getLineSegmentPath = ({previousPoint1, mid1, mid2}) =>
+  Path().moveTo(mid1.x, mid1.y).curveTo(previousPoint1.x, previousPoint1.y, mid2.x, mid2.y)
+
+
+const Snake = React.createClass({
   getInitialState: function() {
     return {
       points: [[]]
@@ -67,11 +97,6 @@ var FireworkShooter = React.createClass({
     isPressing = false
     clearTimeout(pollId)
   },
-  getAlpha: function (i, numberOfPoints) {
-    const threshold =  (numberOfPoints / (numberOfPoints * (numberOfPoints - i)))
-
-    return threshold < 0.09 ? 0 : 1
-  },
   render: function() {
     const lastLine = this.state.points[this.state.points.length - 1]
     const smoothLine = getSmoothLine(lastLine)
@@ -85,9 +110,7 @@ var FireworkShooter = React.createClass({
                 key={i}
                 d={getLineSegmentPath(points)}
                 points={points}
-                strokeCap="round"
-                strokeJoin="miter"
-                opacity={this.getAlpha(i, array.length)}
+                opacity={getAlpha(i, array.length)}
                 stroke="#000"
                 strokeWidth={4}
               />)
@@ -98,27 +121,4 @@ var FireworkShooter = React.createClass({
   }
 })
 
-var getMidPoint = (p1, p2) => ({
-  x: (p1.x + p2.x) * 0.5,
-  y: (p1.y + p2.y) * 0.5
-})
-
-var getSmoothLine = line => line.reduce((acc, cV, i, line) => {
-  const previousPoint2 = line[i]
-  const previousPoint1 = line[i + 1]
-  const currentPoint = line[i + 2]
-
-  if (!currentPoint) {
-    return acc
-  }
-
-  const mid1 = getMidPoint(previousPoint1, previousPoint2)
-  const mid2 = getMidPoint(currentPoint, previousPoint1)
-
-  return acc.concat({previousPoint1, mid1, mid2})
-}, [])
-
-var getLineSegmentPath = ({previousPoint1, mid1, mid2}) =>
-  Path().moveTo(mid1.x, mid1.y).curveTo(previousPoint1.x, previousPoint1.y, mid2.x, mid2.y)
-
-AppRegistry.registerComponent('native_canvas', () => FireworkShooter)
+AppRegistry.registerComponent('native_canvas', () => Snake)
